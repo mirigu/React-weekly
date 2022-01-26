e21b5989426072780a47481c7afd71a9ea334497
MiriGu <miriggu9@gmail.com>
2022-01-26 14:03:19 +0900

    평점 남기기 기능 추가

--- my-weekly-rating/src/Review.js
+++ my-weekly-rating/src/Review.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
 const Review = (props) => {
     let history = useHistory();
-    const {day} = useParams();
-    console.log(day)
+    const { day } = useParams();
+    //console.log(day)
+    const [rate, setRate] = useState(0);
+    console.log(rate)
 
     return (
         <div className="Review">
             <div className="container">
-                <h2>
+                {/* 선택한 요일 보여주기 */}
+                <h2> 
                     <span>{day}요일 </span>
-                        평점 남기기
+                    평점 남기기
                 </h2>
 
                 <div className="circle-box">
-                    {Array.from({ length: 5 }, (circle, index) => {
-                        // console.log(circle)
+                    {Array.from({ length: 5 }, (circle, index) => { // 동그라미 눌러서 평점 입력하기
                         return (
-                            <div  className="circle_review" key = {index}>
+                            <div className="circle_review" key={index} onClick={() => {setRate(index + 1)}} style={{ backgroundColor: rate < index + 1 ? "#C3B3B1" : "#918280" }}>
                             </div>
                         )
                     })}
                 </div>
-                
-                <button onClick={() => {
+
+                <button onClick={() => { //메인페이지로 돌아가기
                     history.goBack();
                 }}>
                     평점 남기기
